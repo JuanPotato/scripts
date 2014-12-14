277da113712d54da6ec67a49107715bf96e702f1
JuanPotato <hasantiny@gmail.com>
2014-12-14 18:29:36 +0200

    Update newemojis.user.js

--- newemojis.user.js
+++ newemojis.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New emojis+ :3
 // @namespace    awkward_potato
-// @version      2.3
+// @version      2.4
 // @description  adds many many more emojis to the oneplus forums
 // @author       awkward_potato, EyeZiS
 // @include      https://forums.oneplus.net/threads/*
