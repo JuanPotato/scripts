d9eb08b4c0c6f97c07f4956bddfe167bf213ec71
awkward_potato / Hasan <hasantiny@gmail.com>
2014-12-16 13:38:58 +0200

    Update modthreads.user.js

--- modthreads.user.js
+++ modthreads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mod threads
 // @namespace    awkward_potato
-// @version      0.3
+// @version      0.4
 // @description  help mod threads while not a mod
 // @author       awkward_potato
 // @include      https://forums.oneplus.net/threads/*
@@ -14,10 +14,24 @@ var frame = document.getElementsByClassName('redactor_textCtrl')[0];
 var body = frame.contentWindow.document.getElementsByTagName('body')[0];
 var begging = $('<li><a class="button">begging</a></li>');
 var likes = $('<li><a class="button">likes</a></li>');
-var dupe = $('<li><a class="button">dupe</a><input type="text" name="dupe" value="search dupe"></li>');
+var dupe = $('<li><a class="button">dupe</a></li>');
+var dupsearch = $('<li><input id="dupsearch" type="text" placeholder="search dupe"></li>')
+$('li.active').before(dupsearch);
 $('li.active').before(dupe);
 $('li.active').before(begging);
 $('li.active').before(likes);
+dupe.click(function(){
+    var dupurl = document.getElementById("dupsearch").value;
+    var token = document.getElementsByName('_xfToken')[0].getAttribute('value')
+    $.post('/conversations/insert', {
+                            keywords:test,
+                            title_only:1,
+                            users:,
+                            date:,
+                            _xfToken:token
+                            });
+    body.innerHTML = 'Duplicate thread<br>Please search before starting a new thread.<br>'+dupurl+body.innerHTML;
+});
 begging.click(function(){
 body.innerHTML = 'Asking for invites is not allowed<br>Please join a thread where they give out invites.<br>'+body.innerHTML;
 });
