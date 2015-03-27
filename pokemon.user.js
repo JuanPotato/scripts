e3f02aa00532184338bde8f3efbe45e89863251a
awkward_potato / Hasan <hasantiny@gmail.com>
2015-03-27 12:56:04 +0200

    added more features

--- pokemon.user.js
+++ pokemon.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         pokemon vortex tools
-// @version      0.2
+// @version      0.3
 // @description  tools, wonderful tools
 // @author       awkward_potato
-// @match        http://zeta.pokemon-vortex.com/*
+// @require      http://code.jquery.com/jquery-1.11.0.min.js
+// @match        *.pokemon-vortex.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -16,46 +17,59 @@ It was very simple once I found out why it wasn't looping as I thought it was
 it only changes the webpage when you start a new battle (I thought on every click :p)
  */
 
+/**********LOGIN SETTING**********/
+var autoLogin = true; //auto-login when logged out
+
+var username = "";
+var password = "";
+
 /**********ON/OFF SETTINGS**********/
-var doBattle = false;
-var findPokemon = true;
-var findLevels = false;
-var findLevelsAndUp = false;
+var doBattle        = false; //enable battles same battle over and over again
+var forceBattle     =  true; //enable being sent to the battle url when at dashboard
+var findPokemon     =  true; //enable finding pokemon alerts
+var findLevels      = false; //enable find specific pokemon levels
+var findLevelsAndUp =  true; //enable finding pokemon levels and up
 
 /**********FINDING SETTINGS**********/
-var pokemonToFind = ["turtwig", "squirtle"];//make sure you get the right spelling
-//TIP: You can also use the above setting as a general keyword searcher in the pokemon you see
+var pokemonToFind = ["turtwig", "squirtle"]; //the pokemon you want to be alerted of
+//make sure you get the right spelling
+//TIP: You can also use the above setting as
+//a general keyword searcher in the pokemon you see
 //e.g. "shiny", "dark"
-var levelsToFind = ["6","3"]; //keep this a string array, 
-var levelsAndUpToFind = 7; //dont make this a string or array
+
+var levelsToFind = ["6","3"]; //The specific levels you want to be alerted for
+//keep this a string array
+
+var levelsAndUpToFind = 50; //level and up to alert about pokemon
+//dont make this a string or array
+
 var scanFreq = 300; //time to wait between every time the script checks if you saw what you wanted (in milliseconds)
 
 /**********FIGHTING SETTINGS**********/
-var battle = "http://zeta.pokemon-vortex.com/battle.php?gymleader=Giovanni";
+var battle = "http://zeta.pokemon-vortex.com/battle.php?gymleader=Brock"; //change this to your battle url
 var attackFreq = 1000; //time to wait between every click while fighting (in milliseconds)
 
 /**********IGNORE EVERYTHING PAST THIS**********/
-var battleUrl = "zeta.pokemon-vortex.com/battle.php";
-var findUrl = "zeta.pokemon-vortex.com/map.php";
+var battleUrl    = "zeta.pokemon-vortex.com/battle.php";
+var findUrl      = "zeta.pokemon-vortex.com/map.php";
+var loginUrl     = "www.pokemon-vortex.com/login.php";
+var dashboardUrl = "zeta.pokemon-vortex.com/dashboard.php";
 
-if (doBattle) {
-    if (window.location.href.indexOf(battleUrl) > -1) {
-        function startBattle() {
-            $("input[value*='Attack']").submit();
-            $("input[value*='Continue']").submit();
-            $("a:contains('Rebattle Opponent')").click();
-        }
-        if (window.location.href == battle) {
-            setInterval(function () {
-                startBattle();
-            }, attackFreq);
-        } else {
-            window.location.href = battle;
-        }
+
+if (doBattle && window.location.href.indexOf(battleUrl) > -1) {
+    function startBattle() {
+        $("input[value*='Attack']").submit();
+        $("input[value*='Continue']").submit();
+        $("a:contains('Rebattle Opponent')").click();
     }
-}
-
-if (findPokemon) {
+    if (window.location.href == battle) {
+        setInterval(function () {
+            startBattle();
+        }, attackFreq);
+    } else {
+        window.location.href = battle;
+    }
+}else if (findPokemon && window.location.href.indexOf(findUrl) > -1) {
     var a;
     var finderOn = false;
     function found(){
@@ -90,19 +104,24 @@ if (findPokemon) {
             }
         }, scanFreq);
     }
-    if (window.location.href.indexOf(findUrl) > -1) {
-        setFinder();
-        $(document).keydown(function(event) {
-            switch (event.keyCode) {
-                case 37: if(!finderOn)setFinder();break;
-                case 38: if(!finderOn)setFinder();break;
-                case 39: if(!finderOn)setFinder();break;
-                case 40: if(!finderOn)setFinder();break;
-                case 87: if(!finderOn)setFinder();break;
-                case 65: if(!finderOn)setFinder();break;
-                case 83: if(!finderOn)setFinder();break;
-                case 68: if(!finderOn)setFinder();break;
-            }
-        });
-    }
+    setFinder();
+    $(document).keydown(function(event) {
+        switch (event.keyCode) {
+            case 37: if(!finderOn)setFinder();break;
+            case 38: if(!finderOn)setFinder();break;
+            case 39: if(!finderOn)setFinder();break;
+            case 40: if(!finderOn)setFinder();break;
+            case 87: if(!finderOn)setFinder();break;
+            case 65: if(!finderOn)setFinder();break;
+            case 83: if(!finderOn)setFinder();break;
+            case 68: if(!finderOn)setFinder();break;
+        }
+    });
+}else if(autoLogin && window.location.href.indexOf(loginUrl) > -1){
+    $("#myusername").val(username);
+    $("#mypassword").val(password);
+    $("input[value*='Log in']").click();
+    $("input[value*='Log in']").submit();
+}else if(forceBattle && window.location.href.indexOf(dashboardUrl) > -1){
+    window.location.href = battle;
 }
